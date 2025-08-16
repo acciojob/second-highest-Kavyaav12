5bdc8ad2133750eb8010c6d85d42ee27bc6d3334
Kavyaav12 <kavyaav100@gmail.com>
2025-08-16 16:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function secondHighest(arr) {
+    if (!Array.isArray(arr) || arr.length < 2) {
+        return -Infinity;
+    }
+
+    let max = -Infinity;
+    let second = -Infinity;
+
+    for (let num of arr) {
+        if (num > max) {
+            second = max;
+            max = num;
+        } else if (num > second && num < max) {
+            second = num;
+        }
+    }
+
+    return (second === -Infinity) ? -Infinity : second;
+}
 //your JS code here. If required.
